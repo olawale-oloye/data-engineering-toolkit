b76b24d2a448d4731cc8d8771cd929c7d0ff28e5
olawale-oloye <olawaleoloye@gmail.com>
2025-09-20 16:27:14 +1000

    ingested data from source(wiki)

---
+++ main.py
@@ -0,0 +1,35 @@
+# Imported Libraries 
+import requests 
+from bs4 import BeautifulSoup 
+import random 
+import pandas as pd
+from io import StringIO
+
+
+user_agents = [
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0",
+        # Add more user agents as needed
+    ]
+
+header = random.choice(user_agents)
+
+url = 'https://en.wikipedia.org/wiki/Human_body_weight'
+
+response = requests.get(url, headers = {'User-Agent':'header'})
+# print(response)
+# print(response.text)
+
+# Use BeautifulSoup to convert plain text to html
+soup = BeautifulSoup(response.text, 'html.parser')
+# print(soup)
+
+# To extract data from the html format data 
+data = soup.find_all('table', {'class':'wikitable'})
+data[0]
+# print(data[0])
+
+# To read_html (convert to DF), soup obj must be converted to a str
+a = pd.read_html(StringIO(str(data[0])))
+print(a[0])
