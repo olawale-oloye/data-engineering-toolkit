5d6e97fbf66382fbac142660b3d1f5e0f9bd8e87
olawale-oloye <olawaleoloye@gmail.com>
2025-09-20 16:44:51 +1000

    data cleaned and plotted

--- main.py
+++ main.py
@@ -50,14 +50,3 @@ df = a[0]
 df.drop(columns='Ref', inplace=True)
 # print(df)
 
-df['Average weight (lb)'] = df['Average weight'].apply(lambda x: float(re.findall(r"\d+\.\d+", x)[1]) )
-df['Average weight (Kg)'] = df['Average weight'].apply(lambda x: float(re.findall(r"\d+\.\d+", x)[0]) )
-df.drop(columns = 'Average weight' , inplace=True)
-df.rename(columns = {'Adult population (millions)': 'Adult population (M)'}, inplace=True)
-df['% Overweight'] = df['% Overweight'].apply(lambda x: float(re.findall(r'\d+\.\d', x)[0]) )
-
-print(df)
-
-sns.barplot(x=df['Region'], y=df['Average weight (Kg)'], hue=df['Region'])
-plt.xticks(rotation= 70)
-plt.show()
